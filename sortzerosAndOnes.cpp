b99a5760601d8c42d8de8775f6c343e0f6d57ef4
Harsh Kumar <149566587+harshk9650@users.noreply.github.com>
2026-05-07 18:47:56 +0530

    Add files via upload

---
+++ sortzerosAndOnes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    
+    vector <int> arr = {1,0,1,0,1,1,0,1,0,0,1,0};
+
+     int start =0;
+     int i=0;
+     int end = arr.size() - 1;
+     
+     while (i<=end)
+     {
+       if(arr[i]==0){
+        swap(arr[i],arr[start]);
+        i++;
+        start++;
+       }else{
+        swap(arr[i],arr[end]);
+        i++;
+        end--;
+       }
+     }
+     
+     for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+     }
+    return 0;
+}
